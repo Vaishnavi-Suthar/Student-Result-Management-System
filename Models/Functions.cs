763878f2600389f3b1e73b2be139ea04bca0cbb4
Vaishnavi <sutharvaishu172@gmail.com>
2025-11-29 10:46:18 +0530

    Add files via upload

---
+++ Models/Functions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SRMS.Models
+{
+    public class Functions
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private string ConStr;
+        private SqlDataAdapter sda;
+
+        public Functions() // Constructor is now public
+        {
+            ConStr = @"Data Source=LAPTOP-HP-ENVY-\SQLEXPRESS;Initial Catalog=SRMS;Integrated Security=True;TrustServerCertificate=True";
+            Con = new SqlConnection(ConStr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+        public DataTable GetDatas(string Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, ConStr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(string Query)
+        {
+            int cnt = 0;
+            if (Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            Con.Close();
+            return cnt;
+        }
+    }
+}
